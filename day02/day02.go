445bcaabbd2fd07da494346ce716ff4f92f18789
Simon Kuhnle <simon@blarzwurst.de>
2023-12-10 21:37:27 +0100

    Clean up day 2 a bit

--- day02/day02.go
+++ day02/day02.go
@@ -16,40 +16,37 @@ const MAX_BLUE int = 14
 
 func main() {
 	file, err := os.Open("input")
-	//file, err := os.Open("sample.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	//valid_games_sum := 0
 	power := 0
-	cube_count_map := map[string]int{}
+	cube_counts := map[string]int{}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
+
+		// Game 31: 8 red, 16 blue; 2 red, 1 green, 1 blue; 5 red, 8 blue, 1 green
 		game := strings.Split(line, ":")
 		sets := strings.Split(game[1], ";")
-		fmt.Println(game[0])
 
 		for _, set := range sets {
-			fmt.Println(set)
-			set = strings.TrimSpace(set)
-			cubes := strings.Split(set, ", ")
-			for _, cube_grabs := range cubes {
-				cube_count := strings.Split(cube_grabs, " ")
-				fmt.Println(cube_count[0], " of ", cube_count[1])
-				count, err := strconv.Atoi(cube_count[0])
+			cube_grabs := strings.Split(strings.TrimSpace(set), ", ")
+			for _, cubes := range cube_grabs {
+				cubes_color_and_count := strings.Split(cubes, " ")
+
+				count, err := strconv.Atoi(cubes_color_and_count[0])
 				if err != nil {
 					panic(err)
 				}
-				if cube_count_map[cube_count[1]] < count {
-					cube_count_map[cube_count[1]] = count
+
+				color := cubes_color_and_count[1]
+
+				if cube_counts[color] < count {
+					cube_counts[color] = count
 				}
 			}
 		}
@@ -57,7 +54,6 @@ func main() {
 		/*
 			if cube_count_map["green"] > MAX_GREEN || cube_count_map["red"] > MAX_RED || cube_count_map["blue"] > MAX_BLUE {
 				fmt.Println("Too many", cube_count_map)
-
 			} else {
 				game_id := strings.Split(game[0], " ")
 				game_id_int, err := strconv.Atoi(game_id[1])
@@ -66,13 +62,12 @@ func main() {
 					panic(err)
 				}
 				valid_games_sum += game_id_int
-				fmt.Println("New sum", valid_games_sum)
 			}
 		*/
-		power += cube_count_map["red"] * cube_count_map["blue"] * cube_count_map["green"]
-		cube_count_map = make(map[string]int)
-
+		power += cube_counts["red"] * cube_counts["blue"] * cube_counts["green"]
+		cube_counts = make(map[string]int)
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
