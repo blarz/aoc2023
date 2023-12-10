8c73acecf6ff034a2b99ddaec118fb989f50e75d
Simon Kuhnle <simon@blarzwurst.de>
2023-12-10 20:45:12 +0100

    Add day 2 part 1, ugly edition...

--- day02/day02.go
+++ day02/day02.go
@@ -1,3 +1,4 @@
+/* First time using Go */
 package main
 
 import (
@@ -5,21 +6,70 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
+const MAX_RED int = 12
+const MAX_GREEN int = 13
+const MAX_BLUE int = 14
+
 func main() {
-	file, err := os.Open("sample.txt")
+	file, err := os.Open("input")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	valid_games_sum := 0
+	cube_count_map := map[string]int{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		game := strings.Split(line, ":")
+		sets := strings.Split(game[1], ";")
+		fmt.Println(game[0])
+
+		for _, set := range sets {
+			fmt.Println(set)
+			set = strings.TrimSpace(set)
+			cubes := strings.Split(set, ", ")
+			for _, cube_grabs := range cubes {
+				cube_count := strings.Split(cube_grabs, " ")
+				fmt.Println(cube_count[0], " of ", cube_count[1])
+				count, err := strconv.Atoi(cube_count[0])
+				if err != nil {
+					panic(err)
+				}
+				if cube_count_map[cube_count[1]] < count {
+					cube_count_map[cube_count[1]] = count
+				}
+			}
+		}
+		if cube_count_map["green"] > MAX_GREEN || cube_count_map["red"] > MAX_RED || cube_count_map["blue"] > MAX_BLUE {
+			fmt.Println("Too many", cube_count_map)
+
+		} else {
+			game_id := strings.Split(game[0], " ")
+			game_id_int, err := strconv.Atoi(game_id[1])
+
+			if err != nil {
+				panic(err)
+			}
+			valid_games_sum += game_id_int
+			fmt.Println("New sum", valid_games_sum)
+		}
+		cube_count_map = make(map[string]int)
+
+	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("SUM", valid_games_sum)
 }
